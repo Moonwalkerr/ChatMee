e9d68790fe2f936f39e5040b625918788ba4b948
Moonwalker <abhisk.mhra@gmail.com>
2021-05-05 19:33:04 +0530

    Sidebar Chat , with seed Api

---
+++ src/comps/SidebarChats.js
@@ -0,0 +1,25 @@
+import {Avatar} from "@material-ui/core";
+import {useState, useEffect} from "react";
+
+
+const SidebarChat = () => {
+    
+    const [seed,setSeed] = useState("");
+
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random()* 5000))
+    },[]);
+
+
+    return ( <div className="sidebarChat">
+      <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+      <div className="sidebarChat__info">
+          <h2>Room Name</h2>
+          <p>Last Message....</p>
+      </div>
+
+    </div> );
+}
+ 
+export default SidebarChat;
