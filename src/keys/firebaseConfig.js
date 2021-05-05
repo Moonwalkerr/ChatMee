37605ce729005f3c05ac09df6c7e44b81aecb8ae
Moonwalker <abhisk.mhra@gmail.com>
2021-05-05 18:04:32 +0530

    firebaseConfig keys

---
+++ src/keys/firebaseConfig.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBuEgI1oevIk-z3yfKJUHP4iwoePupcuQg",
+    authDomain: "folkschat-86e17.firebaseapp.com",
+    projectId: "folkschat-86e17",
+    storageBucket: "folkschat-86e17.appspot.com",
+    messagingSenderId: "438626215482",
+    appId: "1:438626215482:web:ee782c8c31bb9544fb7703"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
