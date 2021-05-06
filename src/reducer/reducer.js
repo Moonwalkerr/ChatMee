dbdb7b4a17adc2ab0ce63e268a06ae4b486b5377
Moonwalker <abhisk.mhra@gmail.com>
2021-05-06 21:40:08 +0530

    reducer File added

---
+++ src/reducer/reducer.js
@@ -0,0 +1,21 @@
+export const initialState = {
+    user: null,
+};
+
+export const action = {
+    SET_USER: "SET_USER",
+};
+
+const reducer = (state, action) =>{
+    switch(action.type){
+        case SET_USER:
+            return {
+                ...state,
+                user: action.user
+
+            }
+    }
+}
+
+
+export default  reducer;
---
+++ src/stateProvider.js
@@ -0,0 +1,14 @@
+import {createContext, useReducer, useContext} from "react";
+
+
+// Preparing the context :- data Layer where every state lives
+export const StateContext = createContext();
+
+// State Provider 
+export const StateProvider = ({ reducer,initialState, children}) =>{
+    <StateContext.Provider value={useReducer(reducer,initialState)}>
+       {children} 
+    </StateContext.Provider>
+}
+
+export const useStateValue = () => useContext(StateContext);
