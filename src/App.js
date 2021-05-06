fa222e43e0d9d0136a7f2dd7a819311c0210d559
Moonwalker <abhisk.mhra@gmail.com>
2021-05-06 19:31:40 +0530

    Routes to / and /app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import ChatDisplay from './comps/chatdisplay';
 import Sidebar from './comps/sidebar';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 // BEM Naming convention
 function App() {
   return (
   <div className="app">
-     {/* <h2>Folks Chat</h2> */}
      <div className="inner__App">
+      <Router>
+      <Switch>
+      <Route path="/app">
       <Sidebar/>
-      <ChatDisplay/>
+      <ChatDisplay/>  
+      </Route>
+      <Route exact path="/">
+        <h1>Home Screen</h1>
+      </Route>
+      </Switch>
+      </Router>
+      
      </div>
     </div>
   );
