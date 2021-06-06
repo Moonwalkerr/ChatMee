76da6ccd86812d973aca051d44c81240b0dc4d34
Moonwalker <abhisk.mhra@gmail.com>
2021-06-06 19:32:52 +0530

    condition to check if rooms are present

--- src/comps/SidebarChats.js
+++ src/comps/SidebarChats.js
@@ -1,58 +1,54 @@
-import {Avatar} from "@material-ui/core";
-import {useState, useEffect} from "react";
+import { Avatar } from "@material-ui/core";
+import { useState, useEffect } from "react";
 import "./sidebarChat.css";
 import database from "../keys/firebaseConfig";
-import {Link} from "react-router-dom";
-const SidebarChat = ({addNewChat,id,name}) => {
-    
-    const [seed,setSeed] = useState("");
-    const [messages, setMessages] = useState("");
+import { Link } from "react-router-dom";
+const SidebarChat = ({ addNewChat, id, name }) => {
+  const [seed, setSeed] = useState("");
+  const [messages, setMessages] = useState("");
 
-    useEffect(() => {
-        setSeed(Math.floor(Math.random()* 5000))
-    },[
-
-    ]);
-    useEffect(()=>{
-            if(id){
-                database.collection("rooms")
-                .doc(id)
-                .collection("messages")
-                .orderBy("timestamp",'desc')
-                .onSnapshot(snap=>(
-                        setMessages(snap.docs.map(doc=>doc.data()))
-                ))
-            }
-    },[id])
-
-    const createChat = () => {
-         const roomName = prompt("Please Enter The New Chat Room Name");
-         if(roomName){
-             database.collection("rooms").add({
-                 name:roomName
-             });
-         }
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+  useEffect(() => {
+    if (id) {
+      database
+        .collection("rooms")
+        .doc(id)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snap) => setMessages(snap.docs.map((doc) => doc.data())));
     }
+  }, [id]);
 
-    if(addNewChat){
-     return  <div onClick={createChat} className="sidebarChat">
-            <h2>Add New Chat +</h2>
-        </div>
+  const createChat = () => {
+    const roomName = prompt("Please Enter The New Chat Room Name");
+    if (roomName) {
+      database.collection("rooms").add({
+        name: roomName,
+      });
     }
+  };
 
-    return ( 
+  if (addNewChat) {
+    return (
+      <div onClick={createChat} className="sidebarChat">
+        <h2>Add New Chat +</h2>
+      </div>
+    );
+  }
+
+  return (
     <Link to={`/rooms/${id}`}>
-    <div
-    key={id}
-    className="sidebarChat">
-      <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
-      <div className="sidebarChat__info">
+      <div key={id} className="sidebarChat">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="sidebarChat__info">
           <h2>{name}</h2>
           <p className="sidebarChat__infoLastmsg">{messages[0]?.message}</p>
+        </div>
       </div>
-    </div> 
-        </Link>
-    );
-}
- 
+    </Link>
+  );
+};
+
 export default SidebarChat;
--- src/comps/sidebar.js
+++ src/comps/sidebar.js
@@ -1,74 +1,77 @@
-import "./sidebar.css"
-import {useState, useEffect} from "react";
-import {IconButton,Avatar} from "@material-ui/core"
-import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import "./sidebar.css";
+import { useState, useEffect } from "react";
+import { IconButton, Avatar } from "@material-ui/core";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import SidebarChat from "./SidebarChats";
 import database from "../keys/firebaseConfig";
-import {useStateValue} from "../stateProvider";
+import { useStateValue } from "../stateProvider";
 import LogoutBtn from "./logoutBtn";
 // import ChatIcon from '@material-ui/icons/Chat';
 // import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 // import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const Sidebar = () => {
+  const [{ user }, dispatch] = useStateValue();
+  dispatch({ type: "noe" });
+  const [rooms, setRooms] = useState([]);
 
-  const [{user},dispatch] = useStateValue();
-  dispatch({type:"noe"});
-    const [rooms, setRooms] = useState([]);
-    
-    useEffect(()=>{     
-        // realtime snapshot update every time data is updated on firebase
-        // Realtime listener and updates the rooms var
-    const unSubscribe = database.collection("rooms").onSnapshot(snap=>{
-                setRooms(snap.docs.map(doc=>({
-                    id:doc.id,
-                    data:doc.data(),
-                })))
-        });
+  useEffect(() => {
+    // realtime snapshot update every time data is updated on firebase
+    // Realtime listener and updates the rooms var
+    const unSubscribe = database.collection("rooms").onSnapshot((snap) => {
+      setRooms(
+        snap.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
 
-        // Cleanup function
-        return ()=>{
-            unSubscribe();
-        }
-    },[])
+    // Cleanup function
+    return () => {
+      unSubscribe();
+    };
+  }, []);
 
-
-    return ( <div className="sidebar">
-        <div className="sidebar__header">
-        <Avatar src={user?user.photoURL:""}/>
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src={user ? user.photoURL : ""} />
         <div className="sidebar__headerRight">
-        {/* <IconButton >
+          {/* <IconButton >
         <ChatIcon style={{display:'none'}}/>
         </IconButton> */}
-        <IconButton>
-        <LogoutBtn/>
-        </IconButton>
-        {/* <IconButton>
+          <IconButton>
+            <LogoutBtn />
+          </IconButton>
+          {/* <IconButton>
         <MoreVertIcon/>    
         </IconButton> */}
-        </div>    
         </div>
-        <div className="sidebar__search">
+      </div>
+      <div className="sidebar__search">
         <div className="sidebar__searchContainer">
-            <SearchOutlinedIcon/>
-        <input type="text" name="" id=""
-        placeholder="Search or Start new Chat"/>
-        
+          <SearchOutlinedIcon />
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Search or Start new Chat"
+          />
         </div>
-        </div>
+      </div>
 
-        <div className="sidebar__chats">
-            <SidebarChat addNewChat/>
-            {rooms.map(room =>{
-                return  <SidebarChat
-                key={room.id}
-                name={room.data.name}
-                id={room.id}
-                />
+      <div className="sidebar__chats">
+        <SidebarChat addNewChat />
+        {rooms &&
+          rooms.map((room) => {
+            return (
+              <SidebarChat key={room.id} name={room.data.name} id={room.id} />
+            );
+          })}
+      </div>
+    </div>
+  );
+};
 
-            })}
-        </div>
-    </div> );
-}
- 
 export default Sidebar;
